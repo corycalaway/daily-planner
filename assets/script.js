905d4ce533b5839d554e55976a7b66fbf9b5369d
corycalaway <cory.c.calaway@gmail.com>
2020-12-01 20:45:35 -0700

    adjusted date to show day month and day name

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 
 var timeEl = document.querySelector("#timeToday")
 
-
-
+// date displayed at top of page
 var setTimeDate = function () {
-    dateTime = luxon.DateTime.local();
-    timeEl.textContent = dateTime;
+   dateTime = luxon.DateTime.local();
+    var dateMonth = {weekday: 'long', month: 'long', day: '2-digit'};
+    timeEl.textContent = dateTime.toLocaleString(dateMonth);
 }
 
 setTimeDate();
