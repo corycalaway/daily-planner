3eb240aa94143551e8e3ff335e5d733f85934c52
corycalaway <cory.c.calaway@gmail.com>
2020-12-02 21:20:01 -0700

    working on text box spacing

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,57 @@
-
 var timeEl = document.querySelector("#timeToday");
 //var timePlace = document.querySelector('#timeHolder');
 var hourIdCounter = 0;
 
-
 // date displayed at top of page
 var setTimeDate = function () {
-   dateTime = luxon.DateTime.local();
-    var dateMonth = {weekday: 'long', month: 'long', day: '2-digit'};
-    timeEl.textContent = dateTime.toLocaleString(dateMonth);
-}
+  dateTime = luxon.DateTime.local();
+  var dateMonth = { weekday: "long", month: "long", day: "2-digit" };
+  timeEl.textContent = dateTime.toLocaleString(dateMonth);
+};
 
 // create time blocks
 var createTimeBlocks = function () {
-    var hourArray = ['5:00 AM', '6:00 AM', '7:00 AM'];
-  //  var timeBlockEl = $('<ul>');
-    //var timeBlockEl = document.createElement('ul')
-    var timeBlockEl = $('<ul></ul>')
-    $('div').append(timeBlockEl);
-    for(var i = 0; i < hourArray.length; i++) {
-   // $.each(hourArray, function(index, value){
+  // time aray
+  var hourArray = [
+    "5:00 AM",
+    "6:00 AM",
+    "7:00 AM",
+    "8:00 AM",
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
+    "5:00 PM",
+  ];
+  // create ul element
+  var timeBlockEl = $("<ul></ul>");
+  // append ul
+  $("div").append(timeBlockEl);
+
+  // cycle through each hour in array and creat li element appended to
+  //for(var i = 0; i < hourArray.length; i++) {
+  $.each(hourArray, function (index, value) {
+    var timeBlockHour = $("<li>")
+    //.addClass("hour").text(hourArray[index]);
+    $(timeBlockEl).append(timeBlockHour);
+
+    var taskRow = $('<div>').addClass('row hour')
+    $(timeBlockHour).append(taskRow);
     
-   
-  var timeBlockHour = $('<li>').addClass('hour');
-  $(timeBlockEl).append(timeBlockHour)
-   // document.div.appendChild(timeBlockEl)
-   // timeBlockEl = className = 'hour';
-   // timeBlockHour.setAttribute('timeCardHour', hourIdCounter)
-    
-    timeBlockHour.textContent = hourArray[i];
-  //  timeBlockEl.append(timeBlockHour);
-    console.log(hourArray)
-   // createTimeBlocks();
-   // hourIdCounter++;
-    
-    }
+
+    var taskTime = $('<div>').addClass('col').text(hourArray[index]);
+    $(taskRow).append(taskTime);
+
+    var taskText = $('<div> <input type="text" class="textarea" </div>').addClass('col');
+    $(taskRow).append(taskText);
+  
+    // hourIdCounter++;
+  });
 };
 
 setTimeDate();
 createTimeBlocks();
-
