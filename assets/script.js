b160d81dc4e78f18f3ddcc3155ebaea3a6f44480
corycalaway <cory.c.calaway@gmail.com>
2020-12-02 19:31:08 -0700

    added list elements

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 
-var timeEl = document.querySelector("#timeToday")
+var timeEl = document.querySelector("#timeToday");
+//var timePlace = document.querySelector('#timeHolder');
+var hourIdCounter = 0;
+
 
 // date displayed at top of page
 var setTimeDate = function () {
@@ -8,6 +11,32 @@ var setTimeDate = function () {
     timeEl.textContent = dateTime.toLocaleString(dateMonth);
 }
 
-setTimeDate();
+// create time blocks
+var createTimeBlocks = function () {
+    var hourArray = ['5:00 AM', '6:00 AM', '7:00 AM'];
+  //  var timeBlockEl = $('<ul>');
+    //var timeBlockEl = document.createElement('ul')
+    var timeBlockEl = $('<ul></ul>')
+    $('div').append(timeBlockEl);
+    for(var i = 0; i < hourArray.length; i++) {
+   // $.each(hourArray, function(index, value){
+    
+   
+  var timeBlockHour = $('<li>').addClass('hour');
+  $(timeBlockEl).append(timeBlockHour)
+   // document.div.appendChild(timeBlockEl)
+   // timeBlockEl = className = 'hour';
+   // timeBlockHour.setAttribute('timeCardHour', hourIdCounter)
+    
+    timeBlockHour.textContent = hourArray[i];
+  //  timeBlockEl.append(timeBlockHour);
+    console.log(hourArray)
+   // createTimeBlocks();
+   // hourIdCounter++;
+    
+    }
+};
 
+setTimeDate();
+createTimeBlocks();
 
