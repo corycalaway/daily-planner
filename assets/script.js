7451c129450cb5121b56459ad459c1321794ef02
corycalaway <cory.c.calaway@gmail.com>
2020-12-05 22:19:14 -0700

    returned first value of textbox

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@ var timeEl = document.querySelector("#timeToday");
 //var timePlace = document.querySelector('#timeHolder');
 var hourIdCounter = 0;
 
- var list = JSON.parse(localStorage.getItem('saveTextList')) || [];
+var list = JSON.parse(localStorage.getItem("saveTextList")) || [];
+console.log(list)
 
 // date displayed at top of page
 var setTimeDate = function () {
@@ -11,43 +12,44 @@ var setTimeDate = function () {
   timeEl.textContent = dateTime.toLocaleString(dateMonth);
 };
 var hourArray = [
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-  ];
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+];
 // create time blocks
 var createTimeBlocks = function () {
   // time aray
-  
+console.log(list)
   // create ul element
- // var timeBlockEl = $("<ul></ul>");
+  // var timeBlockEl = $("<ul></ul>");
   // append ul
- // $("div").append(timeBlockEl);
+  // $("div").append(timeBlockEl);
 
   // cycle through each hour in array and creat li element appended to
   //for(var i = 0; i < hourArray.length; i++) {
   $.each(hourArray, function (index, value) {
-   // var timeBlockHour = $("<li>");
+    // var timeBlockHour = $("<li>");
     //.addClass("hour").text(hourArray[index]);
-   // $(timeBlockEl).append(timeBlockHour);
+    // $(timeBlockEl).append(timeBlockHour);
 
     var taskRow = $("<div>").addClass("row");
-    $('#timeHolder').append(taskRow);
+    $("#timeHolder").append(taskRow);
 
     // creates time values with am/pm
     if (hourArray[index] > 12) {
@@ -68,38 +70,42 @@ var createTimeBlocks = function () {
         .text(hourArray[index] + "PM");
       $(taskRow).append(taskTime);
     }
-//edit from
+    //edit from
     if (dateTime.hour > hourArray[index]) {
-      var taskText = $('<input type="text" class="textarea">').attr('id', 'textId' + hourArray[index]).addClass(
-        "col-10 past anchors"
-        
-      );
+      var taskText = $('<input type="text" class="textarea">')
+        .attr("id", "textId" + hourArray[index])
+        .addClass("col-10 past anchors");
+        $(taskText).val(list[index])
+        console.log()
       $(taskRow).append(taskText);
       // save button
-      var saveTaskBtn = $("<button onclick='saveFullText();'>").attr('id', 'saveButtonTask' + hourArray[index]).addClass("col-1 fa fa-save saveBtn");
-        $(taskRow).append(saveTaskBtn);
+      var saveTaskBtn = $("<button onclick='saveFullText();'>")
+        .attr("id", "saveButtonTask" + hourArray[index])
+        .addClass("col-1 fa fa-save saveBtn");
+      $(taskRow).append(saveTaskBtn);
       //$(taskText).append('<p>')
-// need to add text into text input
-
+      // need to add text into text input
     } else if (dateTime.hour < hourArray[index]) {
-      var taskText = $('<input type="text" class="textarea">').attr('id', 'textId' + hourArray[index]).addClass(
-        "col-10 future anchors"
-    
-      );
+      var taskText = $('<input type="text" class="textarea">')
+        .attr("id", "textId" + hourArray[index])
+        .addClass("col-10 future anchors");
       $(taskRow).append(taskText);
 
       // save button
-      var saveTaskBtn = $("<button onclick='saveFullText();'>").attr('id', 'saveButtonTask' + hourArray[index]).addClass("col-1 fa fa-save saveBtn");
-        $(taskRow).append(saveTaskBtn);
-
+      var saveTaskBtn = $("<button onclick='saveFullText();'>")
+        .attr("id", "saveButtonTask" + hourArray[index])
+        .addClass("col-1 fa fa-save saveBtn");
+      $(taskRow).append(saveTaskBtn);
     } else if ((dateTime.hour = hourArray[index])) {
-      var taskText = $('<input type="text" class="textarea">').attr('id', 'textId' + hourArray[index]).addClass(
-        "col-10 present anchors"
-      );
+      var taskText = $('<input type="text" class="textarea">')
+        .attr("id", "textId" + hourArray[index])
+        .addClass("col-10 present anchors");
       $(taskRow).append(taskText);
       // save button
-      var saveTaskBtn = $("<button onclick='saveFullText();'>").attr('id', 'saveButtonTask' + hourArray[index]).addClass("col-1 fa fa-save saveBtn");
-        $(taskRow).append(saveTaskBtn);
+      var saveTaskBtn = $("<button onclick='saveFullText();'>")
+        .attr("id", "saveButtonTask" + hourArray[index])
+        .addClass("col-1 fa fa-save saveBtn");
+      $(taskRow).append(saveTaskBtn);
       // hourIdCounter++;
     }
 
@@ -117,26 +123,27 @@ createTimeBlocks();
 //     console.log('made it')
 //  });
 
-var saveFullText = function() {
-    console.log('test')
+var saveFullText = function () {
+  console.log("test");
+ list = [];
   $.each(hourArray, function (index, value) {
-
-    var textAnchor = document.getElementsByClassName('anchors')[index].id;
+    var textAnchor = document.getElementsByClassName("anchors")[index].id;
     //console.log(textAnchor)
-    
+
     //  //var textBoxValue = $(this).closest('button').attr('id')
-  var textBoxValue = $('#' + textAnchor)
+    var textBoxValue = $("#" + textAnchor)
+      .val()
+      .trim();
+    console.log(textBoxValue);
 
-  .val()
- .trim();
- console.log(textBoxValue)
-
-//  //.trim()
- list.push(textBoxValue);
- console.log(list)
-})}
-//for ( var i = 0); i 
-
+    //  //.trim()
+    list.push(textBoxValue);
+    console.log(list);
+    
+  });
+  localStorage.setItem('saveTextList', JSON.stringify(list))
+};
+//for ( var i = 0); i
 
 // $.each(hourArray, function (index, value) {
 // //for (i = 0; i < hourArray.length; i++) {
@@ -154,16 +161,11 @@ var saveFullText = function() {
 //  localStorage.setItem('saveTextList', JSON.stringify(list))
 // });})
 
-
-
- // // update createTimeBlocks function with local storage var
+// // update createTimeBlocks function with local storage var
 // localStorage.setItem('saveTextList')
-// // save the 
+// // save the
 // // add the var to local storage
 // //array.push(var)
 // // get value .val() .trim() from text box <input type="text" class="textarea col-10 past">
-
-
-
 
 //use set interval to update colors
